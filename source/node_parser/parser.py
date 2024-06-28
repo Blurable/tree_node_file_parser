88d5e3c2e642d2c385901e321df243eb6b8f0e0b
Artyom <agrhXD@yandex.ru>
2024-06-28 13:26:10 +0300

    parser.py update

--- source/node_parser/parser.py
+++ source/node_parser/parser.py
@@ -14,17 +14,17 @@ class Tokens(Enum):
 class Node:
     counter: int = 0
 
-    def __init__(self, parent: Optional[Node] = None, name: str = '',
+    def __init__(self, parent_id: int = 0, name: str = '',
                   value: Optional[Union[str, list[Node]]] = None):
-        self.parent = parent
+        self.parent_id = parent_id
         self.name = name
         self.value = value
-        self.id = Node.counter
         Node.counter += 1
+        self.id = Node.counter
 
 
     def __str__(self, level = 0):
-        res = f'{'  '*level}({self.id}, {self.parent.id}, {self.name}, '
+        res = f'{'  '*level}({self.id}, {self.parent_id}, {self.name}, '
         match self.value:
             case list():
                 res +=  ' '.join([str(node.id) for node in self.value]) + ')\n'
@@ -38,39 +38,38 @@ class Node:
 class Parser:
     def __init__(self, text: str, include_spaces: bool = False):
         self.tokenizer = Tokenizer(text, include_spaces)
-        self.base_node = Node()
     
 
     def parse(self):
-        node = self.parse_node(self.base_node)
+        node = self.parse_node()
         if self.tokenizer.is_tokens_left():
             raise UnexpectedToken()
         return node
  
 
-    def parse_node(self, parent_node: Node):
+    def parse_node(self, parent_id: int = 0):
         
         name = self.tokenizer.consume(Tokens.NAME_TOKEN.value)
         self.tokenizer.consume(Tokens.EQ_TOKEN.value)
-        cur_node = Node(parent_node, name)
+        cur_node = Node(parent_id, name)
 
         next_token = self.tokenizer.look_up([Tokens.LEFT_BRACE_TOKEN.value, Tokens.VALUE_TOKEN.value])
         match next_token:
             case Tokens.LEFT_BRACE_TOKEN.value:
-                cur_node.value = self.parse_list(cur_node)
+                cur_node.value = self.parse_list(cur_node.id)
             case Tokens.VALUE_TOKEN.value:
                 cur_node.value = self.parse_value()
             
         return cur_node
        
 
-    def parse_list(self, parent_node: Node):
+    def parse_list(self, parent_id: int):
         node_list = []
 
         self.tokenizer.consume(Tokens.LEFT_BRACE_TOKEN.value)
-        node_list += [self.parse_node(parent_node)]
+        node_list += [self.parse_node(parent_id)]
         while self.tokenizer.look_up([token.value for token in Tokens]) != Tokens.RIGHT_BRACE_TOKEN.value:
-            node_list += [self.parse_node(parent_node)]
+            node_list += [self.parse_node(parent_id)]
         self.tokenizer.consume(Tokens.RIGHT_BRACE_TOKEN.value)
 
         return node_list
